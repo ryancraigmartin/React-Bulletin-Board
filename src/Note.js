f4f4dfc2e55794bf5816bf9fa02d7f27bee1db36
Ryan Craig Martin <rmart106@fiu.edu>
2018-06-10 11:04:15 -0400

    Feat: Notes can be updated & removed. Styling changes.

--- src/Note.js
+++ src/Note.js
@@ -15,6 +15,7 @@ class Note extends Component {
     this.renderForm = this.renderForm.bind(this);
     this.renderDisplay = this.renderDisplay.bind(this);
   }
+  
   edit() {
     this.setState({
       editing: true
@@ -22,19 +23,23 @@ class Note extends Component {
   }
 
   remove() {
-    alert("removing note");
+    this.props.onRemove(this.props.index)
   }
 
-  save() {
-    alert(this._newText.value)
+  save(e) {
+    e.preventDefault();
+    this.props.onChange(this._newText.value, this.props.index)
+    this.setState({
+      editing: false
+    })
   }
 
   renderForm() {
     return (
       <div className="note">
-        <form>
+        <form onSubmit={this.save}>
           <textarea ref={input => this._newText = input}/>
-          <button onClick={this.save}>
+          <button onClick={this.save} id="save">
             <FaFloppyO />
           </button>
         </form>
